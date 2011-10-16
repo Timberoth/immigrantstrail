592a589274dde53dba0df3089fdf2f9e821b2bfc
Tim <tim@gamersofaction.org>
2011-10-15 18:48:30 -0700

    Now waiting for input before destroying the text window if autoscroll is disabled.

--- Assets/Scripts/Text/ScrollingTextArea.cs
+++ Assets/Scripts/Text/ScrollingTextArea.cs
@@ -6,9 +6,6 @@ public class ScrollingTextArea : MonoBehaviour {
 	// Number of lines that will fit in a text area.
 	public int maxLines = 3;
 
-	// All the text that will be displayed in this text area.
-	public string text = "This is just some test text that will be replaced later when strings are passed in through a function";
-
 	// Should we wait for player input before moving to the next screen.
 	public bool autoScrolling = true;
 		
@@ -16,6 +13,9 @@ public class ScrollingTextArea : MonoBehaviour {
 	public float scrollSpeed = 1.0f;
 	
 	
+	// All the text that will be displayed in this text area.
+	// Set through the Initialize function.
+	private string text = "SHOULD NEVER BE SEEN!!!";
 	
 	// Track when new words need to be added.
 	private float scrollTimer = 0.0f;
@@ -29,9 +29,11 @@ public class ScrollingTextArea : MonoBehaviour {
 	// Is all the text done scrolling.
 	private bool done = false;
 	
-	// Waiting for a button press
+	// Waiting for a button press to advance to next screen.
 	private bool waitingForInput = false;
 	
+	// Waiting for final button press before firing the callbacks and destroying the text area
+	private bool waitingForFinalInput = false;
 	
 	// GameObject to fire callback on
 	private GameObject callbackObject = null;
@@ -56,7 +58,8 @@ public class ScrollingTextArea : MonoBehaviour {
 		}		
 		
 		// TODO DEBUG
-		Initialize( "Here's some test text", this.gameObject, "AllDone" );
+		Initialize( "Here's some test text that will be so long that it will have to roll over into multiple lines and give us a chance to test the autoscrolling functionality",
+		           this.gameObject, "AllDone" );
 	}
 	
 	void OnDestroy()
@@ -91,7 +94,8 @@ public class ScrollingTextArea : MonoBehaviour {
 		if( done )
 			return;
 		
-		if( waitingForInput )
+		// Waiting for button press to fire callbacks and destroy this object.
+		if( waitingForFinalInput )
 		{
 			bool inputReceived = false;
 #if UNITY_EDITOR
@@ -105,10 +109,35 @@ public class ScrollingTextArea : MonoBehaviour {
 				inputReceived = true;	
 			}
 #endif
+			
+			if( inputReceived )
+			{
+				FinishProcessing();
+			}			
+			return;
+		}
+		
+		
+		// Waiting for button press to advance to next screen.
+		else if( waitingForInput )
+		{
+			bool inputReceived = false;
+#if UNITY_EDITOR
+			if( Input.GetButtonUp("Fire1") )
+			{
+				inputReceived = true;	
+			}
+#elif UNITY_ANDROID || UNITY_IPHONE		
+			if( Input.touches.Length > 0 )
+			{
+				inputReceived = true;	
+			}
+#endif
+			
 			if( inputReceived )
 			{
 				waitingForInput = false;
-				spriteText.Text = "";				
+				spriteText.Text = "";
 			}			
 			return;
 		}
@@ -160,24 +189,42 @@ public class ScrollingTextArea : MonoBehaviour {
 			// We just reached the end of the text.
 			else
 			{			
-				// NEED TO FIGURE OUT WHAT TO DO HERE.
-				done = true;
+				// Don't wait to finish up the text area.
+				if( autoScrolling )
+				{
+					FinishProcessing();
+				}
 				
-				// Fire Callback
-				print("Fire Callback");
-				
-				//GameObject.Destroy( this.gameObject );
-				callbackObject.SendMessage( callbackMessage, "callback test", SendMessageOptions.RequireReceiver);
+				// If autoscroll is turned off, then wait for input before ending
+				// the screen.
+				else
+				{
+					if( !waitingForFinalInput )
+					{
+						// Wait for a button press
+						waitingForFinalInput = true;	
+					}
+				}
 			}
 		}
 	}
 	
 	
+	// Call when the TextArea is done processing and ready to fire off the callbacks
+	// and destroy itslef.
+	void FinishProcessing()
+	{
+		waitingForFinalInput = false;
+		// We're done here.
+		done = true;
+		string DUMMY_DATA = "";
+		callbackObject.SendMessage( callbackMessage, DUMMY_DATA, SendMessageOptions.RequireReceiver);
+		
+		GameObject.Destroy( this.gameObject );	
+	}
+	
 	void AllDone( string data )
 	{
-		print( data );
 		print("All done");
-		
-		GameObject.Destroy( this.gameObject );
 	}
 }
