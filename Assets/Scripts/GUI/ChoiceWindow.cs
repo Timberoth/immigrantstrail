74e6b9aa74631d8fb1cece7f35c9bb0e76977357
Timothy Lambert <consciousgamer@gmail.com>
2011-10-17 18:40:38 -0700

    Basic framework for Choice Window.

---
+++ Assets/Scripts/GUI/ChoiceWindow.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class ChoiceWindow : MonoBehaviour {
+	
+	private GameObject[] choiceObjects;
+	private GameObject questionObject;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		// Grab references to the sub objects within the ChoiceWindow
+		//questionObject
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
